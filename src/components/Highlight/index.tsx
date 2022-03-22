0d6c04c1bfe887a4b39ef6ea9879db78a774dffa
bruno.lebrao1 <bruno.lebrao@memed.com.br>
2022-03-22 20:24:39 -0300

    create Highlight component

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,31 @@
+import { Button } from 'components/Button'
+import * as S from './styles'
+
+export type HighlightProps = {
+  title: string
+  subtitle: string
+  backgroundImage: string
+  floatImage?: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+export const Highlight = ({
+  title,
+  subtitle,
+  backgroundImage,
+  floatImage,
+  buttonLabel,
+  buttonLink
+}: HighlightProps) => (
+  <S.Wrapper backgroundImage={backgroundImage}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+    <S.Content>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Content>
+  </S.Wrapper>
+)
