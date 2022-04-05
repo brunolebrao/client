1b71badfc40d7c0668af991490c8d70c4cda9a39
bruno.lebrao1 <bruno.lebrao@memed.com.br>
2022-04-05 18:19:42 -0300

    create Slider component

---
+++ src/components/Slider/test.tsx
@@ -0,0 +1,24 @@
+import 'match-media-mock'
+import { render, screen } from '@testing-library/react'
+import { Slider } from '.'
+
+describe('<Slider />', () => {
+  it('Should render children as slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    )
+
+    expect(
+      screen.getByText(/Item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(
+      screen.getByText(/Item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
